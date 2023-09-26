325e835b5fa98e84e02d795f20b4b48ce4a26e0f
MelnichenkoSA <112738977+MelnichenkoSA@users.noreply.github.com>
2023-09-26 16:12:06 +0700

    Update Program.cs

--- AVL-Derevo/Program.cs
+++ AVL-Derevo/Program.cs
@@ -4,22 +4,35 @@ Derevo tree = new Derevo();
 
 tree.Insert(50);
 tree.Insert(30);
-tree.Insert(20);
+tree.Insert(35);
+Console.WriteLine(tree.Root.Height);
 tree.Insert(40);
 tree.Insert(70);
+Console.WriteLine(tree.Root.Height);
 tree.Insert(60);
-tree.Insert(80);
+tree.Insert(15);
+Console.WriteLine(tree.Root.Height);
 
 Console.WriteLine("Инфиксный:");
 tree.InorderTraversal();
-Console.WriteLine("\n Префиксный:");
+Console.WriteLine("\nПрефиксный:");
 tree.PreorderTraversal();
-Console.WriteLine("\n Постфиксный:");
+Console.WriteLine("\nПостфиксный:");
 tree.PostorderTraversal();
 
-Console.WriteLine("\nSearching for 30: " + tree.Find(30));
-Console.WriteLine("Searching for 45: " + tree.Find(45));
+Console.WriteLine("\nПоиск 30: " + tree.Find(30));
+Console.WriteLine("Поиск 45: " + tree.Find(45));
 
 tree.Delete(50);
-Console.WriteLine("\nInorder traversal after deleting 30:");
+Console.WriteLine(tree.Root.Height);
+Console.WriteLine("\nИнфиксный после удаления 50:");
 tree.InorderTraversal();
+
+tree.Balance();
+Console.WriteLine(tree.Root.Height);
+Console.WriteLine("\nИнфиксный после баланса:");
+tree.InorderTraversal();
+Console.WriteLine("\nПрефиксный после баланса:");
+tree.PreorderTraversal();
+Console.WriteLine("\nПостфиксный после баланса:");
+tree.PostorderTraversal();
