ccc22d79a12a7632a04f2e0d0902ee3f39cb75e2
melnichenkosa <gerald_mamka_geyba@mail.ru>
2023-12-19 13:13:16 +0700

    Для Левана с любовью

--- AVL-Derevo/Derevo.cs
+++ AVL-Derevo/Derevo.cs
@@ -226,5 +226,101 @@
                 Console.Write(root.Data + " ");
             }
         }
+
+        //Операторы
+        public void PlusTraversal(Node root, Derevo tree2, int b)
+        {
+            if (root != null)
+            {
+                PlusTraversal(root.Left, tree2, b);
+                PlusTraversal(root.Right, tree2, b);
+                tree2.Insert( root.Data + b);
+            }
+        }
+
+        public void PulusTraversal(Node root1, Node root2, Derevo tree3)
+        {
+            Console.WriteLine("*");
+            if ((root1 != null) && (root2 != null))
+            {
+                PulusTraversal(root1.Left, root2.Left, tree3);
+                PulusTraversal(root1.Right, root2.Right, tree3);
+                tree3.Insert(root1.Data);
+                tree3.Insert(root2.Data);
+            }
+
+            else if ((root1 != null) && (root2 == null))
+            {
+
+                PulusTraversal(root1.Left, root1.Left, tree3);
+                PulusTraversal(root1.Right, root1.Right, tree3);
+                tree3.Insert(root1.Data);
+            }
+
+            else if ((root1 == null) && (root2 != null))
+            {
+
+                PulusTraversal(root2.Left, root2.Left, tree3);
+                PulusTraversal(root2.Right, root2.Right, tree3);
+                tree3.Insert(root2.Data);
+            }
+
+        }
+
+        public static Derevo operator +(Derevo a, int b)
+        {
+            Derevo tree2 = new Derevo();
+            a.PlusTraversal(a.Root, tree2, b);
+            return tree2;
+        }
+        public static Derevo operator +(Derevo tree1, Derevo tree2)
+        {
+            if (tree1.MinValue(tree1.Root) >= tree2.MaxValue(tree2.Root))
+            {
+                Derevo tree3 = new Derevo();
+                tree3.Insert(0);
+                tree3.Root.Left = tree2.Root;
+                tree3.Root.Right = tree1.Root;
+                tree3.Delete(0);
+                return tree3;
+            }
+            else if (tree2.MinValue(tree2.Root) >= tree1.MaxValue(tree1.Root))
+            {
+                Derevo tree3 = new Derevo();
+                tree3.Insert(0);
+                tree3.Root.Left = tree1.Root;
+                tree3.Root.Right = tree2.Root;
+                tree3.Delete(0);
+                return tree3;   
+            }
+
+            else
+            {
+                Derevo tree3 = new Derevo();
+
+                tree3.PulusTraversal(tree1.Root, tree2.Root, tree3);
+                return tree3;
+            }
+
+        }
+        /*public static Derevo operator +(Derevo tree1, Derevo tree2)
+        {
+            Derevo tree3 = new Derevo();
+
+            tree3.PulusTraversal(tree1.Root, tree2.Root, tree3);
+            return tree3;
+        }*/
+
+        private int MaxValue(Node root)
+        {
+            int minValue = root.Data;
+            while (root.Right != null)
+            {
+                minValue = root.Right.Data;
+                root = root.Right;
+            }
+            return minValue;
+        }
+
     }
 }
--- AVL-Derevo/Program.cs
+++ AVL-Derevo/Program.cs
@@ -36,3 +36,66 @@ Console.WriteLine("\nПрефиксный после баланса:");
 tree.PreorderTraversal();
 Console.WriteLine("\nПостфиксный после баланса:");
 tree.PostorderTraversal();
+
+
+Derevo tree3 = new Derevo();
+Console.WriteLine();
+tree3 = tree + 2;
+tree.PostorderTraversal();
+Console.WriteLine();
+tree3.PostorderTraversal();
+
+Derevo tree300 = new Derevo();
+Derevo tree400 = new Derevo();
+
+tree300.Insert(50);
+tree300.Insert(30);
+tree300.Insert(35);
+tree300.Insert(40);
+tree300.Insert(70);
+tree300.Insert(60);
+tree300.Insert(15);
+
+tree400.Insert(50);
+tree400.Insert(30);
+tree400.Insert(45);
+tree400.Insert(40);
+tree400.Insert(70);
+tree400.Insert(20);
+tree400.Insert(15);
+tree400.Insert(1);
+
+
+Derevo tree500 = new Derevo();
+
+tree500 = tree300 + tree400;
+tree500.Balance();
+Console.WriteLine("\nСложение");
+Console.WriteLine();
+tree500.PreorderTraversal();
+Console.WriteLine();
+tree500.InorderTraversal();
+Console.WriteLine();
+tree500.PostorderTraversal();
+Console.WriteLine();
+
+Console.WriteLine("/nСложение2");
+
+Derevo tree600 = new Derevo();
+tree600.Insert(1);
+tree600.Insert(2);
+tree600.Insert(6);
+tree600.Insert(7);
+tree600.Insert(5);
+
+Derevo tree700 = new Derevo();
+tree700.Insert(11);
+tree700.Insert(12);
+tree700.Insert(8);
+tree700.Insert(9);
+tree700.Insert(10);
+
+Derevo a = tree600 + tree700;
+Console.WriteLine();
+a.PreorderTraversal();
+
